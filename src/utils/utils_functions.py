da7a00b1ec8f4f9b7c3bb08657192c7bed236f65
bbaumgartnerBX <141214085+bbaumgartnerBX@users.noreply.github.com>
2025-06-11 13:17:46 +0200

    Added  file reading class

--- src/utils/utils_functions.py
+++ src/utils/utils_functions.py
@@ -1,5 +1,6 @@
 import os
 import json
+from uuid import uuid4
 import re
 from langchain_core.prompts import ChatPromptTemplate
 import sys
@@ -8,6 +9,7 @@ sys.path.append("..")
 from agents_src.agent_data_definitions import MetaExpertState
 from textwrap import dedent
 from langchain_core.prompts.prompt import PromptTemplate
+import pandas as pd
 
 
 def get_cwd() -> str:
@@ -279,3 +281,68 @@ def fix_common_spelling_mistakes(domains: list[str]) -> list[str]:
         spelling_fix[domain] if domain in spelling_fix
         else domain for domain in domains
     ]
+
+
+class IMDbReview():
+    def __init__(
+        self,
+        path: str
+    ) -> None:
+        self.path = path
+        self.file_data = self.load_files()
+        self.movies_with_uuid = self.define_movies()
+        
+        self.add_uuid_to_df()
+
+    def load_files(self) -> dict[str, pd.DataFrame]:
+        """
+        Loads the files of the IMDb dataset.
+
+        Args:
+            None.
+
+        Returns:
+            dict[str, str]: Dict with file name as key and
+            corresponding dataframe as values.
+        """
+        files_read = {}
+        files = [
+            f for f in os.listdir(self.path)
+            if os.path.isfile(os.path.join(self.path, f))
+        ]
+        for file in files:
+            files_read[file] = pd.read_json(os.path.join(
+                self.path,
+                file
+            ))[["movie", "review_detail"]]
+        return files_read
+
+    def define_movies(self) -> dict[str, str]:
+        """
+        Extractes the movies from the data and assigns UUID for each.
+
+        Args:
+            None.
+
+        Returns:
+            dict[str, str]: Movie name as key and uuid as value.
+        """
+        all_movies = []
+        for _, df in self.file_data.items():
+            all_movies.extend(df["movie"].to_list())
+            all_movies = list(set(all_movies))
+
+        return {movie: str(uuid4())for movie in all_movies}
+
+    def add_uuid_to_df(self) -> None:
+        """
+        Adds UUID to each dataframe of the class.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        for _, df in self.file_data.items():
+            df['uuid'] = df['movie'].map(self.movies_with_uuid)
